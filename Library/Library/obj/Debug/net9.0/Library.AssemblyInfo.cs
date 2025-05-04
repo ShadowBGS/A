6f7c44f5e0b07399c0a345739168ec09778313d5
Demilade <demiladefajolu@gmail.com>
2025-05-04 22:40:50 +0100

    edit

--- Library/Library/obj/Debug/net9.0/Library.AssemblyInfo.cs
+++ Library/Library/obj/Debug/net9.0/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a164816e20a52ad526675ac23a693a6576987e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c43edd933c372229f408c58de255444938c8f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
